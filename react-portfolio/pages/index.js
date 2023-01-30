3a9211b2dfb828263d170e6cb838c6a0940c8609
CJ <83363242+LinuxGamer@users.noreply.github.com>
2023-01-30 21:41:38 +0000

    Update index.js

--- react-portfolio/pages/index.js
+++ react-portfolio/pages/index.js
@@ -1,4 +1,4 @@
-import Head from 'next/head'
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +11,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
+      <main>
 
       </main>
     </>
